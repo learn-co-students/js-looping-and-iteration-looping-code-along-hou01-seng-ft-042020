3867dbc7bbf133da7a99b7e5c75dc86328263c37
lbirts <birtslauren@gmail.com>
2020-06-01 12:58:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arr, specialEvent) {
+    let thankYouCards = []
+    for (let i = 0; i < arr.length; i++) {
+        thankYouCards.push(`Thank you, ${arr[i]}, for the wonderful ${specialEvent} gift!`);
+        
+    }
+    return thankYouCards
+}
+
+
+function countDown(num) {
+    let count = num;
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+}
