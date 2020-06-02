66311f6363aaca586c63b9580d337352747e2714
Krysta110 <62581900+Krysta110@users.noreply.github.com>
2020-06-02 01:10:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+ 
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+ 
+  return gifts;
+}
+ 
+function writeCards(names, event){
+    let arr = []
+    for (let i=0; i<names.length; i++){
+        arr.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return arr;
+}
+
+function countDown(num){
+    while (num >= 0){
+    console.log(num--);
+    }
+}
+
+
+
+wrapGifts(gifts);
+
+
